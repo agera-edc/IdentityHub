4f675de9f110f05749f00eeb1fee1a2d3137e96b
Marc Gomez <marcgomez@microsoft.com>
2022-06-14 10:15:30 +0200

    Checkstyle

--- extensions/identity-hub/src/main/java/org/eclipse/dataspaceconnector/identityhub/dtos/Status.java
+++ extensions/identity-hub/src/main/java/org/eclipse/dataspaceconnector/identityhub/dtos/Status.java
@@ -21,7 +21,7 @@ abstract class Status {
     private final int code;
     private final String detail;
 
-    public Status(int code, String detail) {
+    Status(int code, String detail) {
         this.code = code;
         this.detail = detail;
     }
