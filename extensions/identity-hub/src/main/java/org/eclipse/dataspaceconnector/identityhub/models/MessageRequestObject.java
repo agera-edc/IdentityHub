a0967f908702da260dc6077891736a477a5d0f69
Ophelie Le Mentec <17216799+ouphi@users.noreply.github.com>
2022-06-20 13:48:11 +0200

    .

--- extensions/identity-hub/src/main/java/org/eclipse/dataspaceconnector/identityhub/models/MessageRequestObject.java
+++ extensions/identity-hub/src/main/java/org/eclipse/dataspaceconnector/identityhub/models/MessageRequestObject.java
@@ -64,7 +64,7 @@ public class MessageRequestObject {
         }
 
         public MessageRequestObject.Builder data(byte[] data) {
-            messageRequestObject.data = data.clone();
+            messageRequestObject.data = data == null ? null : data.clone();
             return this;
         }
 
