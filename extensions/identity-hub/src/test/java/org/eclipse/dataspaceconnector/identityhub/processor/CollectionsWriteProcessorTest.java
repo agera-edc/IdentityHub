07aec630f65e514368e680d1e020378e002f3a8a
Ophelie Le Mentec <17216799+ouphi@users.noreply.github.com>
2022-06-24 14:30:49 +0200

    Fixed writeCredentials test.

--- extensions/identity-hub/src/test/java/org/eclipse/dataspaceconnector/identityhub/processor/CollectionsWriteProcessorTest.java
+++ extensions/identity-hub/src/test/java/org/eclipse/dataspaceconnector/identityhub/processor/CollectionsWriteProcessorTest.java
@@ -20,6 +20,7 @@ import com.github.javafaker.Faker;
 import org.bouncycastle.util.encoders.Base64;
 import org.eclipse.dataspaceconnector.identityhub.models.MessageResponseObject;
 import org.eclipse.dataspaceconnector.identityhub.models.MessageStatus;
+import org.eclipse.dataspaceconnector.identityhub.models.credentials.VerifiableCredential;
 import org.eclipse.dataspaceconnector.identityhub.store.IdentityHubInMemoryStore;
 import org.eclipse.dataspaceconnector.identityhub.store.IdentityHubStore;
 import org.junit.jupiter.api.BeforeEach;
@@ -49,16 +50,18 @@ public class CollectionsWriteProcessorTest {
     @Test
     void writeCredentials() throws JsonProcessingException {
         // Arrange
-        var freeFormJsonMap = Map.of("id", FAKER.internet().uuid(), "number", FAKER.number().digits(10));
-        byte[] data = OBJECT_MAPPER.writeValueAsString(freeFormJsonMap).getBytes(StandardCharsets.UTF_8);
+        var CREDENTIAL_ID = FAKER.internet().uuid();
+        var verifiableCredentialMap = Map.of("id", CREDENTIAL_ID);
+        byte[] data = OBJECT_MAPPER.writeValueAsString(verifiableCredentialMap).getBytes(StandardCharsets.UTF_8);
 
         // Act
         var result = writeProcessor.process(data);
 
         // Assert
         var expectedResult = MessageResponseObject.Builder.newInstance().messageId(MESSAGE_ID_VALUE).status(MessageStatus.OK).build();
+        var expectedVerifiableCredential = VerifiableCredential.Builder.newInstance().id(CREDENTIAL_ID).build();
         assertThat(result).usingRecursiveComparison().isEqualTo(expectedResult);
-        assertThat(identityHubStore.getAll()).usingRecursiveFieldByFieldElementComparator().containsExactly(freeFormJsonMap);
+        assertThat(identityHubStore.getAll()).usingRecursiveFieldByFieldElementComparator().containsExactly(expectedVerifiableCredential);
     }
 
     @Test
