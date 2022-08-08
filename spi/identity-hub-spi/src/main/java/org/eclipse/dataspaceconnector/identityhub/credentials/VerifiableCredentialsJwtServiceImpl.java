f1b16636905fe36b2a2f42ce9e437076ba02853a
Marc Gomez <marcgomez@microsoft.com>
2022-08-08 11:21:37 +0200

    Cleanup

--- spi/identity-hub-spi/src/main/java/org/eclipse/dataspaceconnector/identityhub/credentials/VerifiableCredentialsJwtServiceImpl.java
+++ spi/identity-hub-spi/src/main/java/org/eclipse/dataspaceconnector/identityhub/credentials/VerifiableCredentialsJwtServiceImpl.java
@@ -61,18 +61,18 @@ public class VerifiableCredentialsJwtServiceImpl implements VerifiableCredential
     public Result<Map.Entry<String, Object>> extractCredential(SignedJWT jwt) {
         try {
             var payload = jwt.getJWTClaimsSet().getClaims();
-            monitor.info("Extracting credentials from JWT: " + payload);
             var vcObject = payload.get(VERIFIABLE_CREDENTIALS_KEY);
             if (vcObject == null) {
                 return Result.failure(String.format("No %s field found", VERIFIABLE_CREDENTIALS_KEY));
             }
             var verifiableCredential = objectMapper.convertValue(vcObject, VerifiableCredential.class);
 
-            monitor.info("Extracting credentials from JWT done");
+            monitor.debug("Extracted credentials from JWT");
+
             return Result.success(new AbstractMap.SimpleEntry<>(verifiableCredential.getId(), payload));
         } catch (ParseException | RuntimeException e) {
-            monitor.info("Failure extracting credentials from JWT", e);
-            return Result.failure(Objects.requireNonNullElseGet(e.getMessage(), () -> e.toString()));
+            monitor.severe("Failure extracting credentials from JWT", e);
+            return Result.failure(Objects.requireNonNullElseGet(e.getMessage(), e::toString));
         }
     }
 }
